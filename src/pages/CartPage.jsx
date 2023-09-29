8f809cec5c29fc3665945060ded03044297cfca4
“milliybro” <shohrux-rustamov@mail.ru>
2023-09-29 13:53:19 +0500

    fdfdf

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,14 +1,40 @@
 import React, { Component, Fragment } from 'react'
+import axios from "axios";
+import ProductsCard from '../components/card/ProductsCard';
+
 
 export class CartPage extends Component {
+   state = {
+      categories: [],
+    };
+    async getCart() {
+      try {
+        let { data } = await axios(
+          `https://fakestoreapi.com/products/category/men's clothing`
+        );
+        this.setState({ categories: data });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  
+    componentDidMount() {
+      this.getCart();
+    }
   render() {
+   const { categories } = this.state;
     return (
       <Fragment>
-         <section>
-            <div className="container">
-               Cart
+        <section>
+          <div className="container">
+            <h1 className="mt-5">Carts</h1>
+            <div className="products-row my-3">
+              {categories.map((category) => (
+                <ProductsCard key={category.title} {...category} />
+              ))}
             </div>
-         </section>
+          </div>
+        </section>
       </Fragment>
     )
   }
